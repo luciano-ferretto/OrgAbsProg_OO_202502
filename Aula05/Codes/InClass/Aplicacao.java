e4738d537031528644d89e4f4ce480b0ca5f164b
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2025-09-10 23:00:53 -0300

    Final Aula06

--- Aula05/Codes/InClass/Aplicacao.java
+++ Aula05/Codes/InClass/Aplicacao.java
@@ -2,17 +2,38 @@ import java.time.LocalDate;
 
 public class Aplicacao {
     public static void main(String[] args) {
+        Integer numInteger = 1001; //Cria um objeto Integer (ex. @11) com o value=1001,
+                                    // e a variável recebe essa referência (@11)
+                                    // numInteger ==> Imutável -- Objeto não muda --
+        
+        numInteger = 2222; //Como numInteger é Imutável, 
+                            //cria-se um novo Objeto Integer (ex. @22) com o value=2222, 
+                            // e a variável recebe essa referência (@22)
+                            
+                            //O objeto anterior (@11) fica órfão,
+                            // e é destruído com o Garbage Collector
+
+
         Veiculo focus; //Declara uma variável "focus" do tipo "Veiculo"
         focus = new Veiculo();   //invoca o método construtor "Veiculo()"
                                 //atribuo um novo objeto (veículo) para variável "focus"
-        focus.marca = "Ford";
-        focus.modelo = "Focus";
-        focus.placa = "ABC-1234";
-        focus.ano = 2018;
+        
+        //focus.marca = "Ford";
+        focus.setMarca("Ford");
+        //focus.modelo = "Focus";
+        focus.setModelo("Focus");
+        //focus.placa = "ABC-1234";
+        focus.setPlaca("ABC-1234");
+        //focus.ano = 2018;
+        focus.setAno(2018);
 
-        int anoAtual = LocalDate.now().getYear();
-        int tempoUsoFocus = focus.calculaTempoUso(anoAtual);
-        System.out.println("Tempo de uso do " + focus.modelo + ": " + tempoUsoFocus);
+        int tempoUsoFocus = focus.calculaTempoUso();
+        System.out.println("Tempo de uso do " + focus.getModelo() + ": " + tempoUsoFocus);
+
+
+        Veiculo newFocus = focus.cloneMe();
+        newFocus.ano = 2024;
+
 
         Veiculo corolla = new Veiculo();
         corolla.marca = "Toyota";
@@ -23,8 +44,13 @@ public class Aplicacao {
         Veiculo newCorolla = corolla;
         newCorolla.ano = 2025;
 
-        corolla.calculaTempoUso(anoAtual);
-        newCorolla.calculaTempoUso(anoAtual);
+        corolla.calculaTempoUso(2030);
+        newCorolla.calculaTempoUso(2030);
+
+
+        Veiculo fusca = new Veiculo("Volkswagem", "Fusca", "DEF-9999", 1995);
+        fusca.setMarca("Volks");
+        System.out.println(fusca.getMarca());
 
     }
 }
